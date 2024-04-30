2e30e007abf3e4055af3be4cdcd69f50d810aad7
ayuwidianti13 <ayuwdnti13@gmail.com>
2024-04-30 16:00:50 +0800

    Kumpul Tugas 6

---
+++ H071221056/Praktikum-6/app/src/main/java/com/example/pertemuan6/DetailResponse.java
@@ -0,0 +1,16 @@
+package com.example.pertemuan6;
+
+import java.util.List;
+
+public class DetailResponse {
+
+    private User data;
+
+    public User getData() {
+        return data;
+    }
+
+    public void setData(User data) {
+        this.data = data;
+    }
+}
